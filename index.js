f5a6740c79c98f70d5634aedd5b8813e28e2ea70
Max Mat <mmatronin@gmail.com>
2025-11-30 01:38:14 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 const express = require("express");
 const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(express.json());
